99f9dbc58cc498917300d03536d80e6542557d2d
Pooja955 <pratyashasatapathy79@gmail.com>
2019-07-29 00:24:12 +0530

    thirdcommit

---
+++ src/Login.java
@@ -0,0 +1,40 @@
+//Pratyasha satapathy java utility
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *039 Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 *039 @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 039 Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("username");
+		String pass=request.getParameter("password");
+		if(pass.contentEquals("pooja")) {
+			RequestDispatcher rd=request.getRequestDispatcher("servlet2");
+			rd.forward(request,response);
+		}
+		else {
+			out.println("sorry username and password Error");
+			RequestDispatcher rd=request.getRequestDispatcher("/index.html");
+			rd.include(request,responce;)
+			}
+		}
+
+}
